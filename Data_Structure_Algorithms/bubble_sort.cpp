40598eb75e69dcca017da886e84bb025d553b473
mashfiqur11 <mashfiqur07@gmail.com>
2025-04-27 16:41:20 +0600

    Adding bubble sort code

---
+++ Data_Structure_Algorithms/bubble_sort.cpp
@@ -0,0 +1,31 @@
+/// Implement Bubble Sort algorithm
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() 
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    int swaps = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-1; j++)
+        {
+            if (arr[j] > arr[j+1])  
+            {
+                swap (arr[i], arr[i+1]);
+                swaps++;
+            }
+        }
+    }
+
+    cout << "Array is sorted in " << swaps << "swap" << endl;
+    for (int i = 0; i < n; i++) cout << arr[i] << " "; cout << endl;
+
+    return 0;
+}
